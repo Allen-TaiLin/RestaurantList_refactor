bcbf58525ed47b6a7b13ac0ad231ed761f951fb1
Allen-TaiLin <skygary2000@gmail.com>
2020-11-17 10:24:10 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
+// require packages used in the projects
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const port = 3000
+
+//設定連線字串
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 
 app.get('/', (req, res) => {
   res.send('restaurant')

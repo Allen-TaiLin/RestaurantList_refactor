9aecbde4797470973cd60ab239a0a5a256bac5d9
Allen-TaiLin <skygary2000@gmail.com>
2021-01-16 20:19:00 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,9 @@ const restaurants = require('./modules/restaurants')
 const search = require('./modules/search')
 // 引入 sort 模組程式碼
 const sort = require('./modules/sort')
+// 引入 users 模組程式碼
+const users = require('./modules/users')
+
 // 將網址結構符合 / 字串的 request 導向 home 模組
 router.use('/', home)
 // 將網址結構符合 /restaurants 字串開頭的 request 導向 restaurants 模組
@@ -18,5 +21,8 @@ router.use('/restaurants', restaurants)
 router.use('/search', search)
 // 將網址結構符合 /sort 字串的 request 導向 sort 模組
 router.use('/sort', sort)
+// 將網址結構符合 /users 字串的 request 導向 sort 模組
+router.use('/users', users)
+
 // 匯出路由器
 module.exports = router

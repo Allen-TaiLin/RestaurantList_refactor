ebcc8acfc4607ce2c0dddea6bc9c34522fbebbb6
Allen-TaiLin <skygary2000@gmail.com>
2021-09-14 20:23:35 +0800

    modify restaurantSeeder.js for reset user Data

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -30,32 +30,50 @@ const restaurantList = require('../../restaurant.json').results
 //新增種子資料
 db.once('open', () => {
   return Promise.all(SEED_USERS.map((item) => {
-    bcrypt
-      .genSalt(10)
-      .then((salt) => bcrypt.hash(item.password, salt))
-      .then((hash) => User.create({
-        name: item.name,
-        email: item.email,
-        password: hash
-      }))
-      .then((user) => {
-        if (user.name === 'Tom') {
-          return Promise.all(Array.from({ length: 3 }, (item, index) => {
-            console.log(user)
-            return RestaurantData.create({ ...restaurantList[index], userId: user._id })
-          }))
-        } else {
-          return Promise.all(Array.from({ length: 3 }, (item, index) => {
-            console.log('user2', user)
-            return RestaurantData.create({ ...restaurantList[index + 3], userId: user._id })
-          }))
-        }
+    //查詢種子資料是否已在資料庫
+    User.find({ email: item.email })
+      .then((haveUser) => {
+        //const userArray = [...haveUser] //find的回傳結果已經是Array
+        console.log(haveUser)
+        console.log('***')
+        //已存在就刪除舊的種子資料
+        haveUser.forEach((oldItem) => {
+          Promise.all([RestaurantData.deleteMany({ userId: oldItem._id }), User.deleteOne({ _id: oldItem._id })])
+          console.log(`已清除：編號 ${oldItem._id} 使用者的相關資料`)
+        })
       })
-      .then(() => {
-        console.log('Data insert done.')
-        process.exit()
-      })
+      .then(
+        bcrypt
+          .genSalt(10)
+          .then((salt) => bcrypt.hash(item.password, salt))
+          .then((hash) => User.create({ //寫入使用者的種子資料
+            name: item.name,
+            email: item.email,
+            password: hash
+          }))
+          .then((user) => {
+            console.log('開始準備匯入餐廳的種子資料')
+            if (user.name === 'Tom') {
+              return Promise.all(Array.from({ length: 3 }, (item, index) => {
+                console.log('user1-', user)
+                return RestaurantData.create({ ...restaurantList[index], userId: user._id })
+              }))
+            } else {
+              return Promise.all(Array.from({ length: 3 }, (item, index) => {
+                console.log('user2-', user)
+                return RestaurantData.create({ ...restaurantList[index + 3], userId: user._id })
+              }))
+            }
+          })
+          .then(() => {
+            console.log('Data insert done.')
+            process.exit()
+          })
+          .catch((error) => console.log(error))
+      )
       .catch((error) => console.log(error))
+
+
   }))
 })
 
